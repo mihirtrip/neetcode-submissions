c4856d2b3f20eebdceec912c2a4c43fe82e99002
Mihir Tripathi <107728582+mihirtrip@users.noreply.github.com>
2026-06-14 22:53:23 +0530

    Add: pacific-atlantic-water-flow - submission-0

---
+++ Data Structures & Algorithms/pacific-atlantic-water-flow/submission-0.cpp
@@ -0,0 +1,61 @@
+class Solution {
+public:
+    int m, n;
+    vector<pair<int,int>> dir = {{1,0}, {-1,0}, {0,1}, {0,-1}};
+
+    void dfs(int r, int c, vector<vector<int>>& heights,
+             vector<vector<bool>>& vis) {
+
+        vis[r][c] = true;
+
+        for(auto &[dr, dc] : dir) {
+            int nr = r + dr;
+            int nc = c + dc;
+
+            if(nr < 0 || nc < 0 || nr >= m || nc >= n)
+                continue;
+
+            if(vis[nr][nc])
+                continue;
+
+            if(heights[nr][nc] < heights[r][c])
+                continue;
+
+            dfs(nr, nc, heights, vis);
+        }
+    }
+
+    vector<vector<int>> pacificAtlantic(vector<vector<int>>& heights) {
+
+        m = heights.size();
+        n = heights[0].size();
+
+        vector<vector<bool>> pacific(m, vector<bool>(n, false));
+        vector<vector<bool>> atlantic(m, vector<bool>(n, false));
+
+        for(int j = 0; j < n; j++) {
+            dfs(0, j, heights, pacific);
+        }
+        for(int i = 0; i < m; i++) {
+            dfs(i, 0, heights, pacific);
+        }
+        for(int j = 0; j < n; j++) {
+            dfs(m - 1, j, heights, atlantic);
+        }
+        for(int i = 0; i < m; i++) {
+            dfs(i, n - 1, heights, atlantic);
+        }
+
+        vector<vector<int>> ans;
+
+        for(int i = 0; i < m; i++) {
+            for(int j = 0; j < n; j++) {
+                if(pacific[i][j] && atlantic[i][j]) {
+                    ans.push_back({i, j});
+                }
+            }
+        }
+
+        return ans;
+    }
+};
