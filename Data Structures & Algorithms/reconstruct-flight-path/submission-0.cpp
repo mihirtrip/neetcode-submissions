7fe0f83cd2729711e8943af458a64edd90c18389
Mihir Tripathi <107728582+mihirtrip@users.noreply.github.com>
2026-06-18 22:42:40 +0530

    Add: reconstruct-flight-path - submission-0

---
+++ Data Structures & Algorithms/reconstruct-flight-path/submission-0.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    unordered_map<string,
+        priority_queue<string, vector<string>, greater<string>>> adj;
+
+    vector<string> ans;
+
+    void dfs(string src) {
+        while (!adj[src].empty()) {
+            string next = adj[src].top();
+            adj[src].pop();
+
+            dfs(next);
+        }
+
+        ans.push_back(src);
+    }
+
+    vector<string> findItinerary(vector<vector<string>>& tickets) {
+
+        for (auto &ticket : tickets) {
+            adj[ticket[0]].push(ticket[1]);
+        }
+
+        dfs("JFK");
+
+        reverse(ans.begin(), ans.end());
+
+        return ans;
+    }
+};
