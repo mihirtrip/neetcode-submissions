84e6135e14e7c4b3abf603a3b3a81ed5a921b2c0
Mihir Tripathi <107728582+mihirtrip@users.noreply.github.com>
2026-06-20 22:20:30 +0530

    Add: swim-in-rising-water - submission-0

---
+++ Data Structures & Algorithms/swim-in-rising-water/submission-0.cpp
@@ -0,0 +1,54 @@
+class Solution {
+public:
+    int swimInWater(vector<vector<int>>& grid) {
+        int n = grid.size();
+
+        priority_queue<
+            vector<int>,
+            vector<vector<int>>,
+            greater<vector<int>>
+        > pq;
+
+        vector<vector<bool>> vis(n, vector<bool>(n, false));
+
+        pq.push({grid[0][0], 0, 0});
+
+        vector<pair<int, int>> dir = {
+            {1, 0}, {-1, 0}, {0, 1}, {0, -1}
+        };
+
+        while (!pq.empty()) {
+            auto curr = pq.top();
+            pq.pop();
+
+            int time = curr[0];
+            int r = curr[1];
+            int c = curr[2];
+
+            if (vis[r][c])
+                continue;
+
+            vis[r][c] = true;
+
+            if (r == n - 1 && c == n - 1)
+                return time;
+
+            for (auto &[dr, dc] : dir) {
+                int nr = r + dr;
+                int nc = c + dc;
+
+                if (nr < 0 || nc < 0 || nr >= n || nc >= n)
+                    continue;
+
+                if (vis[nr][nc])
+                    continue;
+
+                int newTime = max(time, grid[nr][nc]);
+
+                pq.push({newTime, nr, nc});
+            }
+        }
+
+        return -1;
+    }
+};
